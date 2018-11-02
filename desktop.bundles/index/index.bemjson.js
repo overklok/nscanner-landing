6107dc14b688b45cb9d9bd2d4bfc28b7dc4c906b
borland <john_overklok@yahoo.com>
2018-11-02 09:48:36 +0500

    title

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,6 +1,6 @@
 module.exports = {
     block: 'page',
-    title: 'Title of the page',
+    title: 'N-scanner.ru',
     favicon: '/favicon.ico',
     head: [
         {elem: 'meta', attrs: {name: 'description', content: ''}},
